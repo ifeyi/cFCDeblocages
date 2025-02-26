67e214cbb3365be7453e52ae13a123514d9aa945
Ifeyibatindek <ifeyibatindek@gmail.com>
2025-02-26 12:26:22 +0100

    Mise à jour codes déblocages

--- cfcdeblocages/cfcdeblocages/settings.py
+++ cfcdeblocages/cfcdeblocages/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -123,6 +124,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'cfcdeblocagesmiseenplace/static/')
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
