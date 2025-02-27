f3689d2f780aa40ef933229e3199f69c674bdfa4
Ifeyibatindek <ifeyibatindek@gmail.com>
2025-02-27 07:33:55 +0100

    added a gitignore file

--- cfcdeblocages/cfcdeblocagesmiseenplace/views.py
+++ cfcdeblocages/cfcdeblocagesmiseenplace/views.py
@@ -1,12 +1,25 @@
-from django.shortcuts import render
-from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
 from django.contrib import messages
+from .utils import deblocage_authenticate
 
 def home(request):
-    return render(request, 'home.html', {})
 
-def login_user(request):
-    pass
+    if request.method == 'POST':
+        deblocage_usermail = request.POST['deblocage-usermail']
+        deblocage_password = request.POST['deblocage-password']
+
+        user = deblocage_authenticate(email=deblocage_usermail, password=deblocage_password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'Vous êtes connecté')
+            return redirect('home')
+        else:
+            messages.error(request, 'Identifiants incorrects')
+            return redirect('home')
+    else:
+        return render(request, 'home.html', {})
 
 def logout_user(request):
     pass
